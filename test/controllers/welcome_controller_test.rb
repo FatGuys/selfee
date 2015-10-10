b267c39a72e919da89e6ab70dcdd6c347174f76c
Mike Tineo <mtineo@zendesk.com>
2015-10-10 02:17:49 +0100

    Assets

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -6,9 +6,4 @@ class WelcomeControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get login" do
-    get :login
-    assert_response :success
-  end
-
 end
